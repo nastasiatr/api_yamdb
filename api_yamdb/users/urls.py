f44508a64d11620b9efb34d481e9e4c0a0ef8095
Анастасия Трусова <trusovaanastasia@mail.ru>
2023-04-07 16:07:30 +0800

    1.0

--- api_yamdb/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from users.views import APIGetToken, APISignup
-
-urlpatterns = [
-    path('token/', APIGetToken.as_view(), name='get_token'),
-    path('signup/', APISignup.as_view(), name='signup'),
-]
